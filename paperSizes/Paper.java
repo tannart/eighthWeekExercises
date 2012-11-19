9664fe9796b9a8c7bfe177037dcfdf36322107f3
charliembrodie <cbrodi01@cspc055.dcs.bbk.ac.uk>
2012-11-19 17:05:12 +0000

    this is fubar

--- paperSizes/Paper.java
+++ paperSizes/Paper.java
@@ -1,7 +1,9 @@
 public class Paper{
     
 	public void getSize(String paper){
+	    if(paper.charAt(1 == "0"){
 	    String str = paper.substring(1);
 		int sizeName = Integer.parseInt(str);
+		if(
 	}
 }
