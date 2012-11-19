279355c6063a688fa9fd1c3e558886c9a23c5b76
charliembrodie <cbrodi01@cspc055.dcs.bbk.ac.uk>
2012-11-19 14:40:59 +0000

    See Recursion.java.

---
+++ lineByLine/RecursionTest.java
@@ -0,0 +1,15 @@
+public class RecursionTest{
+
+    public static void main(String[] args){
+	
+	
+	    Recursion tester = new Recursion();
+		
+		tester.printNumbers(6);
+		//tester.doggyMethod(6);
+		System.out.println(tester.mcCarthy91(50));
+		System.out.println(tester.mcCarthy91(73));
+		System.out.println(tester.mcCarthy91(95));
+		
+	}
+}
