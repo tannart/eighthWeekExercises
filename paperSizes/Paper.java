8eb85a1c51639075410fa8a8a02407c328a79663
Charlie Brodie <charliebrodie@Charlie-Brodies-MacBook-Pro.local>
2012-11-21 09:50:41 +0000

    Against all odds I have managed to get functionality from the 0 sizes of the paper  getSize method, the relief is unimaginable, however unfortunately my socks are still soaking.

--- paperSizes/Paper.java
+++ paperSizes/Paper.java
@@ -1,9 +1,47 @@
 public class Paper{
-    
-	public void getSize(String paper){
-	    if(paper.charAt(1 == "0"){
-	    String str = paper.substring(1);
-		int sizeName = Integer.parseInt(str);
-		if(
+ int size1 = 841;
+ int size2 = 1189;
+
+	public String getSize(String paper){
+		int size;
+		String paperSize;
+		String firstNum;
+		String secondNum;
+ 		int sizeHolder = 0;
+
+ 		if(paper.length() == 2 && paper.charAt(1) == '0'){
+
+ 			sizeHolder = size1*2;
+			size1 = size2;
+			size2 = sizeHolder;
+
+			paperSize = size1 + "mm x " + size2 + "mm";
+	    	return paperSize;
+
+ 		}else if(paper.length() > 2){
+
+			sizeHolder = size1*2;
+			size1 = size2;
+			size2 = sizeHolder;
+
+			System.out.println(sizeHolder);
+
+			return getSize(paper.substring(0,(paper.length()-1)));
+
+		} else {
+
+			size = Integer.parseInt(paper.substring(1));		
+
+			for(int i = size ; i < 0; i--){
+
+				sizeHolder = size2/2;
+				size2 = size1;
+				size1 = sizeHolder;
+			}
+
+		paperSize = size1 + "mm x " + size2 + "mm";
+	    return paperSize;
+
+		}	
 	}
 }
