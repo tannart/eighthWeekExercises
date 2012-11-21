0a37069ec4d0b0932febc904ba2d3c0a8d95bd0a
charliembrodie <cbrodi01@cspc062.dcs.bbk.ac.uk>
2012-11-21 17:07:11 +0000

    I am very close to completing this exercise, however the palindrome recognition functionality is still not perfect.

---
+++ mainPalindrome.java
@@ -0,0 +1,11 @@
+public class mainPalindrome{
+	
+	public static void main(String[] args){
+		
+		Palindrome test = new Palindrome();
+		
+		test.isPalindrome("japan");
+		test.isPalindrome("england");
+		test.isPalindrome("civic");
+	}
+}
