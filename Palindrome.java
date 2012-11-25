40e847b0c7caf5c208074c4b7ed84095ece83bf8
Charlie Brodie <charliebrodie@Charlie-Brodies-MacBook-Pro.local>
2012-11-25 13:30:22 +0000

    I have now completed this exercise with full functionality.

---
+++ Palindrome.java
@@ -0,0 +1,30 @@
+public class Palindrome{
+
+	String word;
+    
+	public static String isRecursivePalindrome(String word){
+		if(word.length() == 1){
+		    return word;
+		} else {
+			char letterHolder = word.charAt(0);
+			return isRecursivePalindrome(word.substring(1)) + letterHolder;
+		}
+	}
+
+	public String isPalindrome(String word){
+	    String backword = isRecursivePalindrome(word);
+
+		if(backword.equals(word)){		
+
+			System.out.println(backword + " is the same as " + word + " congratulations, you now have a palindrome");
+			System.out.println("");
+
+		} else {
+
+			System.out.println(backword + " is not the same as " + word + " you fool, no palindrome for you");
+			System.out.println("");
+		}
+
+		return backword;
+	}
+}
